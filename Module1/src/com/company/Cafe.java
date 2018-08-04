303130f146fc14c934f889a2b9559ca21f9dae0a
Sanjeev <thedevelopersanjeev@gmail.com>
2018-08-05 04:01:43 +0530

    basic application added

---
+++ Module1/src/com/company/Cafe.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public final class Cafe {
+
+    private int beansInStock = 0;
+    private int milkInStock = 0;
+
+    public Coffee brew(CoffeeType coffeeType, int quantity){
+        requirePositive(quantity);
+
+        int requiredBeans = coffeeType.getRequiredBeans() * quantity;
+        int requiredMilk = coffeeType.getRequiredMilk() * quantity;
+
+        if(requiredBeans > beansInStock || requiredMilk > milkInStock){
+            throw new IllegalStateException("Insufficient beans or milk");
+        }
+
+        beansInStock -= requiredBeans;
+        milkInStock -= requiredMilk;
+
+        return new Coffee(coffeeType, requiredBeans, requiredMilk);
+    }
+
+    public void restockBeans(int weightInGrams){
+        requirePositive(weightInGrams);
+        beansInStock += weightInGrams;
+    }
+
+    public void restockMilk(int weightInGrams){
+        requirePositive(weightInGrams);
+        milkInStock += weightInGrams;
+    }
+
+    private void requirePositive(int value){
+        if(value < 1)
+            throw new IllegalStateException();
+    }
+
+    public int getBeansInStock() {
+        return beansInStock;
+    }
+
+    public int getMilkInStock() {
+        return milkInStock;
+    }
+}
---
+++ Module1/src/com/company/CoffeeType.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public enum CoffeeType {
+    Espresso(7, 0),
+    Latte(7, 227),
+    FilterCoffee(10, 0);
+
+    private final int requiredBeans;
+    private final int requiredMilk;
+
+    CoffeeType(int requiredBeans, int requiredMilk){
+        this.requiredBeans = requiredBeans;
+        this.requiredMilk = requiredMilk;
+    }
+
+    public int getRequiredBeans() {
+        return requiredBeans;
+    }
+
+    public int getRequiredMilk() {
+        return requiredMilk;
+    }
+}
